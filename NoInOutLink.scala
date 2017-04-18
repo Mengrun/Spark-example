05acd625b5fcd99f7db6a9e95ab4f4b88830c992
Mengrun <mengrun.li@duke.edu>
2017-04-18 15:39:20 -0400

    Update NoInOutLink.scala

--- NoInOutLink.scala
+++ NoInOutLink.scala
@@ -4,15 +4,13 @@ import org.apache.log4j.Level
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-// Do NOT use different Spark libraries.
+
 
 object NoInOutLink {
     def main(args: Array[String]) {
         val input_dir = "sample_input"
         val links_file = input_dir + "/links-simple-sorted.txt"
         val titles_file = input_dir + "/titles-sorted.txt"
-        //val links_file = "links-simple-sorted.txt"
-        //val titles_file = "titles-sorted.txt"
         val num_partitions = 10
         
 
@@ -37,9 +35,6 @@ object NoInOutLink {
                     .map{case (u,v) => (u.toString,v.toString)}
 
 
-        frompage.collect().foreach(println)
-        //topage.collect().foreach(println)
-
         val titles = sc
             .textFile(titles_file, num_partitions)
         val pair = titles.zipWithIndex
@@ -57,19 +52,14 @@ object NoInOutLink {
 
 
         /* No Outlinks */
-        //val outlink_bc = outlink.collect.toSet
-        //val no_outlinks = indexKey.filter{case (u, v) => !outlink_bc.contains(u,v)}
         val no_outlinks = indexKey.subtractByKey(outlink)
         println("[ NO OUTLINKS ]")
-        //no_outlinks.take(10).foreach(println)
         no_outlinks.takeOrdered(10)(Ordering[Int].on(x => x._1.toInt)).foreach(println)
 
         /* No Inlinks */
-        //val inlink_bc = inlink.collect.toSet
-        //val no_inlinks = indexKey.filter{case(u,v) => !inlink_bc.contains(u,v)}
+
         val no_inlinks = indexKey.subtractByKey(inlink)
         println("\n[ NO INLINKS ]")
-        //no_inlinks.take(10).foreach(println)
         no_inlinks.takeOrdered(10)(Ordering[Int].on(x => x._1.toInt)).foreach(println)
     }
 }
